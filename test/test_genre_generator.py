65b8487784670afa02a5bf156f7ff2840e6b3a60
Chaam Zeina <zeinachaam@gmail.com>
2024-07-31 08:43:01 +0100

    added unit tests for loading model and checking if output is from training

--- test/test_genre_generator.py
+++ test/test_genre_generator.py
@@ -1,6 +1,7 @@
 import unittest
 from parameterized import parameterized
 from src.genre_generator import GenreGenerator
+from typing import Optional
 
 
 class TestGenreGenerator(unittest.TestCase):
@@ -30,9 +31,49 @@ class TestGenreGenerator(unittest.TestCase):
         ]
     )
     def test_parse_output_invalid(self, input: str):
-        generator = GenreGenerator(path_to_training="./test_data/training_data.csv")
+        generator = GenreGenerator(path_to_training="./test/test_data/training_data.csv")
 
         self.assertRaises(ValueError, generator._parse_output, input)
+
+    @parameterized.expand(
+        [
+            (("musical genre", True)),
+            (("slowcore", False)),
+            (("genre example", True)),
+            (("example of of genre", False)),
+            (("musical category", True)),
+        ]
+    )
+    def test_is_output_from_training(self, output: str, expected_bool: str):
+        generator = GenreGenerator(path_to_training="./test/test_data/training_data.csv")
+        real_bool = generator._is_output_from_training(output)
+
+        self.assertEqual(real_bool, expected_bool)
+
+    def test_load_model_sets_model_and_tokenizer(self):
+        generator = GenreGenerator(path_to_tuned_model = "microsoft/phi-1_5", 
+                                   tokenizer_name = "microsoft/phi-1_5", 
+                                   path_to_training="./test/test_data/training_data.csv")
+        generator._load_model()
+        
+        self.assertTrue(hasattr(generator, "model"))
+        self.assertTrue(hasattr(generator, "tokenizer"))
+
+# TODO: generate requires valid output format, so will likely need to test with actual model
+    # @parameterized.expand(
+    #     [
+    #         ((1,)),
+    #         ((None,)),
+    #     ]
+    # )
+    # def test_retry_with_new_seed_generates_new_string_if_valid_or_no_input(self, input: Optional[int]):
+    #     generator = GenreGenerator(path_to_tuned_model = "microsoft/phi-1_5", 
+    #                                tokenizer_name = "microsoft/phi-1_5", 
+    #                                path_to_training="./test/test_data/training_data.csv")
+    #     generator._load_model()
+    #     output = generator._retry_with_new_seed(input)
+
+    #     self.assertIsInstance(output, str)
 
 
 if __name__ == "__main__":
